be18910f9431805d338767c66b59d23dd19cd904
DunHou <769867389@qq.com>
2022-09-17 13:42:02 +0800

    master

--- Redshift node API examples.py
+++ Redshift node API examples.py
@@ -28,6 +28,21 @@ doc = c4d.documents.GetActiveDocument()
 #=============================================
 #                  Examples
 #=============================================
+def GetTextureInBrowser(): # 从AssetsBrowser获取贴图URL
+
+    assetId: maxon.Id = maxon.Id("file_68991a1ba2bbef15")
+    
+    repository: maxon.AssetRepositoryRef = maxon.AssetInterface.GetUserPrefsRepository()
+    asset: maxon.AssetDescription = repository.FindLatestAsset(
+        maxon.AssetTypes.File(), assetId, maxon.Id(), maxon.ASSET_FIND_MODE.LATEST)
+    name: str = asset.GetMetaString(maxon.OBJECT.BASE.NAME, maxon.Resource.GetCurrentLanguage(), "")
+
+    url: maxon.Url = asset.GetUrl()
+    fileName: str = url.GetUrl()
+    print(url)
+    print(fileName)
+    return url
+
 if rs.RedshiftNodeBased(): # 检测是否为Redshift Node Base
 
     #---------------------------------------------------------
@@ -50,11 +65,11 @@ if rs.RedshiftNodeBased(): # 检测是否为Redshift Node Base
     # 新建节点 修改属性
     # Modify Standard Surface
     #---------------------------------------------------------
-    
+
     def AddandModify(name):
 
         redshiftMaterial = rs.CreateStandardSurface(name)
-        
+
         # modification has to be done within a transaction
         with rs.RSMaterialTransaction(redshiftMaterial) as transaction:
 
@@ -72,7 +87,12 @@ if rs.RedshiftNodeBased(): # 检测是否为Redshift Node Base
             color_mix_node = redshiftMaterial.AddShaderTo("rscolormix", rsID.StrPortID("rscolormix","outcolor"), standard_surface, base_color)
             ramp_node = redshiftMaterial.AddRamp()
             cc_node = redshiftMaterial.AddColorCorrectTo(color_mix_node,mix_input1)
-            tex_node = redshiftMaterial.AddTexture('YourTex', 'abc', 'RS_INPUT_COLORSPACE_RAW')
+            # texPath
+            # 贴图路径
+            url = GetTextureInBrowser()
+            # Add a Texture node and set a tex to it , change color space to RAW
+            # 添加一个Texture shader , 设置贴图路径,并将色彩空间设置为RAW
+            tex_node = redshiftMaterial.AddTexture('YourTex', url, 'RS_INPUT_COLORSPACE_RAW')
             # connect a shader's port to another shader's port
             # 添加链接
             redshiftMaterial.AddConnection(noise_node, noise_out, color_mix_node, mix_input2)
@@ -90,7 +110,7 @@ if rs.RedshiftNodeBased(): # 检测是否为Redshift Node Base
             # 查看属性
             print("standard_surface AssetID = {0}".format(redshiftMaterial.GetAssetId(standard_surface)))
             print("standard_surface ShaderID = {0}".format(redshiftMaterial.GetShaderId(standard_surface)))
-            print("roughness Value = {0}".format(redshiftMaterial.GetShaderValue(standard_surface,roughness)))            
+            print("roughness Value = {0}".format(redshiftMaterial.GetShaderValue(standard_surface,roughness)))
             print("Connections Num = {0}".format(len(connectioninfo)))
             for i, element in enumerate(connectioninfo):
                 print(("Connections" + str(i) + "= {0}".format(connectioninfo[i])))
@@ -107,8 +127,8 @@ if rs.RedshiftNodeBased(): # 检测是否为Redshift Node Base
         CreateStandard("1.Standard Surface")
         # --- 2 --- #
         example2 = AddandModify("2.Modify Material")
-        
+
     c4d.EventAdd()
-    
+
 else:
     c4d.gui.MessageDialog("Redshift Node Material is not Valuable")
